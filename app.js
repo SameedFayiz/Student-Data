0bfc4de755c0c0e7472534636545149faf5d5338
Muhammad Sameed Fayiz <sameedfayiz@gmail.com>
2023-06-04 23:51:46 +0500

    Uploaded project files

---
+++ app.js
@@ -0,0 +1,418 @@
+var userResult = [
+    {
+        name: "John Doe",
+        results: [
+            { subject: "Math", marks: 85 },
+            { subject: "English", marks: 92 },
+            { subject: "Science", marks: 78 }
+        ]
+    },
+    {
+        name: "Jane Smith",
+        results: [
+            { subject: "Math", marks: 90 },
+            { subject: "English", marks: 88 },
+            { subject: "Science", marks: 76 }
+        ]
+    },
+    {
+        name: "Alice Johnson",
+        results: [
+            { subject: "Math", marks: 82 },
+            { subject: "English", marks: 95 },
+            { subject: "Science", marks: 88 }
+        ]
+    },
+    {
+        name: "Robert Davis",
+        results: [
+            { subject: "Math", marks: 78 },
+            { subject: "English", marks: 82 },
+            { subject: "Science", marks: 79 }
+        ]
+    },
+    {
+        name: "Emily Wilson",
+        results: [
+            { subject: "Math", marks: 92 },
+            { subject: "English", marks: 86 },
+            { subject: "Science", marks: 94 }
+        ]
+    },
+    {
+        name: "Michael Brown",
+        results: [
+            { subject: "Math", marks: 87 },
+            { subject: "English", marks: 90 },
+            { subject: "Science", marks: 83 }
+        ]
+    },
+    {
+        name: "Olivia Taylor",
+        results: [
+            { subject: "Math", marks: 79 },
+            { subject: "English", marks: 91 },
+            { subject: "Science", marks: 84 }
+        ]
+    },
+    {
+        name: "James Anderson",
+        results: [
+            { subject: "Math", marks: 86 },
+            { subject: "English", marks: 84 },
+            { subject: "Science", marks: 90 }
+        ]
+    },
+    {
+        name: "Sophia Martinez",
+        results: [
+            { subject: "Math", marks: 93 },
+            { subject: "English", marks: 79 },
+            { subject: "Science", marks: 88 }
+        ]
+    },
+    {
+        name: "Benjamin Thomas",
+        results: [
+            { subject: "Math", marks: 88 },
+            { subject: "English", marks: 83 },
+            { subject: "Science", marks: 91 }
+        ]
+    },
+    // Continue adding more students...
+    {
+        name: "Emma Wilson",
+        results: [
+            { subject: "Math", marks: 85 },
+            { subject: "English", marks: 92 },
+            { subject: "Science", marks: 78 }
+        ]
+    },
+    {
+        name: "William Johnson",
+        results: [
+            { subject: "Math", marks: 90 },
+            { subject: "English", marks: 88 },
+            { subject: "Science", marks: 76 }
+        ]
+    },
+    {
+        name: "James Smith",
+        results: [
+            { subject: "Math", marks: 78 },
+            { subject: "English", marks: 82 },
+            { subject: "Science", marks: 79 }
+        ]
+    },
+    {
+        name: "Olivia Wilson",
+        results: [
+            { subject: "Math", marks: 92 },
+            { subject: "English", marks: 86 },
+            { subject: "Science", marks: 94 }
+        ]
+    },
+    {
+        name: "Michael Johnson",
+        results: [
+            { subject: "Math", marks: 87 },
+            { subject: "English", marks: 90 },
+            { subject: "Science", marks: 83 }
+        ]
+    },
+    {
+        name: "Emma Davis",
+        results: [
+            { subject: "Math", marks: 79 },
+            { subject: "English", marks: 91 },
+            { subject: "Science", marks: 84 }
+        ]
+    },
+    {
+        name: "William Anderson",
+        results: [
+            { subject: "Math", marks: 86 },
+            { subject: "English", marks: 84 },
+            { subject: "Science", marks: 90 }
+        ]
+    },
+    {
+        name: "Ava Martinez",
+        results: [
+            { subject: "Math", marks: 93 },
+            { subject: "English", marks: 79 },
+            { subject: "Science", marks: 88 }
+        ]
+    },
+    {
+        name: "James Thomas",
+        results: [
+            { subject: "Math", marks: 88 },
+            { subject: "English", marks: 83 },
+            { subject: "Science", marks: 91 }
+        ]
+    },
+    {
+        name: "Daniel Johnson",
+        results: [
+            { subject: "Math", marks: 78 },
+            { subject: "English", marks: 80 },
+            { subject: "Science", marks: 75 }
+        ]
+    },
+    {
+        name: "Liam Wilson",
+        results: [
+            { subject: "Math", marks: 87 },
+            { subject: "English", marks: 89 },
+            { subject: "Science", marks: 82 }
+        ]
+    },
+    {
+        name: "Jacob Anderson",
+        results: [
+            { subject: "Math", marks: 79 },
+            { subject: "English", marks: 87 },
+            { subject: "Science", marks: 84 }
+        ]
+    },
+    {
+        name: "Olivia Martinez",
+        results: [
+            { subject: "Math", marks: 91 },
+            { subject: "English", marks: 83 },
+            { subject: "Science", marks: 92 }
+        ]
+    },
+    {
+        name: "Ethan Thomas",
+        results: [
+            { subject: "Math", marks: 86 },
+            { subject: "English", marks: 90 },
+            { subject: "Science", marks: 81 }
+        ]
+    },
+    {
+        name: "Isabella Wilson",
+        results: [
+            { subject: "Math", marks: 83 },
+            { subject: "English", marks: 92 },
+            { subject: "Science", marks: 87 }
+        ]
+    },
+    {
+        name: "Mason Johnson",
+        results: [
+            { subject: "Math", marks: 89 },
+            { subject: "English", marks: 84 },
+            { subject: "Science", marks: 93 }
+        ]
+    },
+    {
+        name: "Ava Davis",
+        results: [
+            { subject: "Math", marks: 85 },
+            { subject: "English", marks: 88 },
+            { subject: "Science", marks: 90 }
+        ]
+    },
+    {
+        name: "William Smith",
+        results: [
+            { subject: "Math", marks: 87 },
+            { subject: "English", marks: 83 },
+            { subject: "Science", marks: 85 }
+        ]
+    },
+    {
+        name: "Sophia Brown",
+        results: [
+            { subject: "Math", marks: 88 },
+            { subject: "English", marks: 85 },
+            { subject: "Science", marks: 86 }
+        ]
+    },
+    {
+        name: "Benjamin Taylor",
+        results: [
+            { subject: "Math", marks: 91 },
+            { subject: "English", marks: 90 },
+            { subject: "Science", marks: 92 }
+        ]
+    },
+    {
+        name: "Abigail Johnson",
+        results: [
+            { subject: "Math", marks: 89 },
+            { subject: "English", marks: 87 },
+            { subject: "Science", marks: 88 }
+        ]
+    },
+    {
+        name: "Logan Wilson",
+        results: [
+            { subject: "Math", marks: 83 },
+            { subject: "English", marks: 85 },
+            { subject: "Science", marks: 84 }
+        ]
+    },
+    {
+        name: "Charlotte Martinez",
+        results: [
+            { subject: "Math", marks: 87 },
+            { subject: "English", marks: 90 },
+            { subject: "Science", marks: 86 }
+        ]
+    },
+    {
+        name: "Lucas Thomas",
+        results: [
+            { subject: "Math", marks: 88 },
+            { subject: "English", marks: 82 },
+            { subject: "Science", marks: 90 }
+        ]
+    },
+    {
+        name: "Harper Davis",
+        results: [
+            { subject: "Math", marks: 85 },
+            { subject: "English", marks: 87 },
+            { subject: "Science", marks: 84 }
+        ]
+    },
+    {
+        name: "Jackson Anderson",
+        results: [
+            { subject: "Math", marks: 90 },
+            { subject: "English", marks: 83 },
+            { subject: "Science", marks: 89 }
+        ]
+    }
+    // Continue adding more students...
+];
+
+
+//Sorting Funcions
+
+function sortByAlpha() {
+    let newArray = userResult.sort((a, b) => { return a.name.charCodeAt(0) - b.name.charCodeAt(0) });
+    return newArray;
+};
+
+function sortByMaths() {
+    let newArray = userResult.sort((a, b) => { return b.results[0].marks - a.results[0].marks });
+    return newArray;
+};
+
+function sortByEnglish() {
+    let newArray = userResult.sort((a, b) => { return b.results[1].marks - a.results[1].marks });
+    return newArray;
+};
+
+function sortByScience() {
+    let newArray = userResult.sort((a, b) => { return b.results[2].marks - a.results[2].marks });
+    return newArray;
+};
+
+function sortByTotal() {
+    let newArray = userResult.sort((a, b) => {
+        let total1 = b.results[0].marks + b.results[1].marks + b.results[2].marks;
+        let total2 = a.results[0].marks + a.results[1].marks + a.results[2].marks;
+        return total1 - total2;
+    })
+    return newArray;
+};
+
+// Placing Student Data in HTML
+
+function printData(stdArray = [], reversed = false) {
+    var tBody = document.getElementById("stdData");
+    tBody.innerHTML = null;
+    reversed = eval(document.querySelector("input[name=order]:checked").value);
+    if (reversed == true) {
+        stdArray = stdArray.reverse();
+    };
+    for (let i = 0; i < stdArray.length; i++) {
+        var stdData = stdArray[i];
+        var math = stdData.results[0].marks;
+        var eng = stdData.results[1].marks;
+        var sci = stdData.results[2].marks;
+        var total = math + eng + sci;
+        var std = `<tr>
+                    <th scope="row">${i + 1}</th>
+                    <td class="stdName">${stdData.name}</td>
+                    <td>${math}</td>
+                    <td>${eng}</td>
+                    <td>${sci}</td>
+                    <td>${math + eng + sci}</td>
+                    <td>${(total * 100 / 300).toFixed(2)}</td>
+                </tr>`;
+        tBody.innerHTML += std;
+    };
+    let position = tBody.children;
+    if (!reversed) {
+        position[0].className = "table-primary";
+        position[1].className = "table-info";
+        position[2].className = "table-success";
+    } else {
+        let n = position.length
+        position[n - 1].className = "table-primary";
+        position[n - 2].className = "table-info";
+        position[n - 3].className = "table-success";
+    }
+    return null;
+};
+
+// Initializing the HTML
+
+printData(sortByAlpha());
+
+// Event Listeners for Sorting Fucntions
+
+function sortFunc() {
+    let sort = document.getElementById("sortBy").value;
+    if (sort == "0") {
+        printData(sortByAlpha());
+    } else if (sort == "1") {
+        printData(sortByMaths());
+    } else if (sort == "2") {
+        printData(sortByEnglish());
+    } else if (sort == "3") {
+        printData(sortByScience());
+    } else if (sort == "4") {
+        printData(sortByTotal());
+    }
+}
+
+// Search Implementation
+
+function searchStd() {
+    let found = false
+    let stdToFind = document.getElementById("searchStd").value;
+    let tBody = document.getElementsByClassName("stdName");
+    for (let i = 0; i < tBody.length; i++) {
+        if (tBody[i].innerText == stdToFind) {
+            found = true
+            let std = tBody[i].parentElement.children;
+            let stdName = std[1].textContent;
+            let maths = std[2].textContent;
+            let eng = std[3].textContent;
+            let sci = std[4].textContent;
+            let total = std[5].textContent;
+            Swal.fire({
+                title: `Student Found!`,
+                text: `Name: ${stdName} | Maths: ${maths} | English: ${eng} | Science: ${sci} | Total Marks: ${total} | Percentage: ${(total * 100 / 300).toFixed(2)}`,
+                icon: 'success',
+                confirmButtonText: 'Okay'
+            });
+            return null;
+        }
+    }
+    if (!found) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Student not found!',
+        })
+    }
+};
